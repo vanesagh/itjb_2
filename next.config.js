ade3c116aeb9205652773cd75635086419e2491e
vanesagh <vanesag@chalmers.se>
2023-05-19 17:28:08 -0600

    last part of session 5

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains:["d33wubrfki0l68.cloudfront.net"],
+  }
 }
 
 module.exports = nextConfig
--- src/pages/projects/[id]/index.js
+++ src/pages/projects/[id]/index.js
@@ -1,10 +1,25 @@
 import PageDescription from "@/components/PageDescription";
-
+import Image from "next/image";
+import Link from "next/link";
 import { Box, Grid, Button, Chip } from "@mui/material";
+import { NextSeo } from "next-seo";
 
 function Project({project}){
     return (
     <>
+    <NextSeo 
+       title={project.name}
+       description={project.description}
+       openGraph={{
+        title:"Title",
+        description:"Description of the project",
+        url:"google.com",
+        type:"websites",
+        image:"https://d33wubrfki0l68.cloudfront.net/19c708670a1f21231c1e2efa6ba38dbf52b15343/3237e/assets/jpeg/dopefolio.jpeg",
+       }}
+    >
+        
+    </NextSeo>
       <Grid 
         container
         direction="column"
@@ -23,7 +38,12 @@ function Project({project}){
             </Button>
         </Box>
         <Box>
-            <img src={project.imageUrl} alt="sd"/>
+            <Image 
+              src={project.imageUrl}
+              width={900}
+              height={550}
+              alt="image project"
+            />
         </Box>
         <h1> Project Overview</h1>
         <Box>
@@ -31,33 +51,60 @@ function Project({project}){
         </Box>
         <h1>Tools used</h1>
         <Box sx={{display:"flex", flexWrap:"wrap", gap:1}}>
-            {project.tools.map((tool)=>(
-                <Chip 
-                  key={tool} 
-                  label={tool} 
-                  variant="outlined"
-                />
+            {project.tools.map((tool)=>{
+                return (
+                    <Chip
+                        key={tool}
+                        label={tool}
+                        variant="outlined" />
 
-            ))}
+                );
+            })}
         </Box>
+        <Link href="/projects">
+            <Button variant="contained" size="large">
+                Go back
+            </Button>
+        </Link>
+        
       </Grid>
     </>
     );
 }
-export async function getServerSideProps(context){
-    const id = context.params.id;
+export async function getStaticPaths(){
+    
     try {
-        const response = await fetch("http://localhost:3000/api/projects/"+id);
-        const project = await response.json();
-        return{
-            props:{
-                project
-            }
-        }
+        const response = await fetch(`http://localhost:3000/api/projects/`);
+        const projects = await response.json();
+        const paths = projects.map((project)=>{
+            return {params:{id:project._id.toString()}}
+        });
+        return {
+            paths,
+            fallback: "blocking",
+        };
         
     } catch (error) {
         console.error(error);
         
     }
+}
+
+export async function getStaticProps({params}){
+    try {
+        const response = await fetch(`http://localhost:3000/api/projects/${params.id}`)
+        const project = await response.json()
+        return {
+            props:{
+                project,
+            },
+            revalidate: 5
+        };
+    } catch (error) {
+        return {notFound: true};
+        
+    }
+
+
 }
 export default Project;
